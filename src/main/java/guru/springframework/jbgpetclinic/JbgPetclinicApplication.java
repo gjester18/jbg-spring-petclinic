340a151ca9891ab131f5611ac5f51e8351364529
jbgutierrez <gjester18@gmail.com>
2018-11-30 15:16:40 +0800

    initial project commit

---
+++ src/main/java/guru/springframework/jbgpetclinic/JbgPetclinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.jbgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbgPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbgPetclinicApplication.class, args);
+    }
+}
---
+++ src/test/java/guru/springframework/jbgpetclinic/JbgPetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.jbgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JbgPetclinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
