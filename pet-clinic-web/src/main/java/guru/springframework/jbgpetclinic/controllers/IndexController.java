ce12d6ab0a31181cc17527c83dff13f78324b403
jbgutierrez <gjester18@gmail.com>
2018-12-01 22:36:28 +0800

    modified indexcontroller for mvcn release prepare

--- pet-clinic-web/src/main/java/guru/springframework/jbgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/jbgpetclinic/controllers/IndexController.java
@@ -1,7 +1,8 @@
 package guru.springframework.jbgpetclinic.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class IndexController {
 
 
