d28a92afd6debef0be086b9229e77edfd4d0afcd
jbgutierrez <gjester18@gmail.com>
2018-12-02 15:01:19 +0800

    list all owners in owners index. Closes #15.

--- pet-clinic-web/src/main/java/guru/springframework/jbgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/jbgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package guru.springframework.jbgpetclinic.controllers;
 
+import guru.springframework.jbgpetclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index"})
-    public String ownerList(){
+    public String ownerList(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
